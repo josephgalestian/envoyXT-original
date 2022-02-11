2f1c9297d58c2cb9d76810da11437c3fc3e2ba49
josephgalestiyan@gmail.com <josephgalestiyan@gmail.com>
2022-02-11 13:19:49 -0800

    'initial'

---
+++ envoy-main/source/server/config_validation/admin.cc
@@ -0,0 +1,31 @@
+#include "source/server/config_validation/admin.h"
+
+namespace Envoy {
+namespace Server {
+
+// Pretend that handler was added successfully.
+bool ValidationAdmin::addHandler(const std::string&, const std::string&, HandlerCb, bool, bool) {
+  return true;
+}
+
+bool ValidationAdmin::removeHandler(const std::string&) { return true; }
+
+const Network::Socket& ValidationAdmin::socket() { return *socket_; }
+
+ConfigTracker& ValidationAdmin::getConfigTracker() { return config_tracker_; }
+
+void ValidationAdmin::startHttpListener(const std::list<AccessLog::InstanceSharedPtr>&,
+                                        const std::string&,
+                                        Network::Address::InstanceConstSharedPtr,
+                                        const Network::Socket::OptionsSharedPtr&,
+                                        Stats::ScopePtr&&) {}
+
+Http::Code ValidationAdmin::request(absl::string_view, absl::string_view, Http::ResponseHeaderMap&,
+                                    std::string&) {
+  PANIC("not implemented");
+}
+
+void ValidationAdmin::addListenerToHandler(Network::ConnectionHandler*) {}
+
+} // namespace Server
+} // namespace Envoy
